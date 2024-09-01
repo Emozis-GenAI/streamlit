9174a12c299e7ab4a27246e7a18687eb631c2fb0
narae3759 <narae3759@gmail.com>
2024-09-01 22:58:49 +0900

    Feat: 비밀번호 변경 페이지 추가

--- pages/login.py
+++ pages/login.py
@@ -53,6 +53,14 @@ if st.session_state["login_window"] == "login":
             args=["register"],
             key="register"
         )
+        # 비밀번호 리셋 버튼
+        st.button(
+            label="비밀번호 리셋",
+            use_container_width=True, 
+            on_click=login_window, 
+            args=["reset"],
+            key="reset"
+        )
 # 회원가입 화면
 elif st.session_state["login_window"] == "register":
     columns = st.columns(spec=[0.2,0.6,0.2])
@@ -76,7 +84,30 @@ elif st.session_state["login_window"] == "register":
             st_textbox("닉네임은 영문, 한글, 숫자만 가능하며, 1자 이상 입력하세요.", fontsize=0.7, style="list")
             st_textbox("비밀번호는 영문, 숫자, 특수문자만 가능하며, 4자 이상 입력하세요.", fontsize=0.7, style="list")
             vertical_space(2)
+# 비밀번호 변경 화면
+elif st.session_state["login_window"] == "reset":
+    columns = st.columns(spec=[0.2,0.6,0.2])
+    with columns[1]:
+        with st.form(key="login"):
+            # 제목
+            st.markdown("비밀번호 변경")
+            # 입력 창
+            username = st.text_input(label="ID", max_chars=15)
+            nickname = st.text_input(label="닉네임", max_chars=15)
+            password = st.text_input(label="비밀번호", max_chars=15, type="password")
+            re_password = st.text_input(label="비밀번호 확인", max_chars=15, type="password")
+            # 비밀번호 변경 버튼
+            reset_btn = st.form_submit_button(
+                label="비밀번호 변경", 
+                use_container_width=True, 
+                type="primary"
+            )
+            # 주의사항
+            st_textbox("비밀번호는 영문, 숫자, 특수문자만 가능하며, 4자 이상 입력하세요.", fontsize=0.7, style="list")
+            vertical_space(2)
 
+
+            
 # if st.session_state["login_window"] == "login":
 #     columns = st.columns(spec=[0.2,0.6,0.2])
 #     with columns[1]:
