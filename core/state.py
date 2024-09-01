6daecc510a5ee6e5367cdb2ed1dbdd172715483d
narae3759 <narae3759@gmail.com>
2024-09-01 20:28:34 +0900

    Feat: 기본 사용 메서드 구현
    - Config, yaml 읽고 딕셔너리로 구현하는 과정 추가
    - Logger, loguru를 통한 로거 기능 구현
    - Utils, CSS, 스타일 관련 코드 추가

---
+++ core/state.py
@@ -0,0 +1,4 @@
+import streamlit as st 
+
+def login_window(window):
+    st.session_state["login_window"] = window
---
+++ core/utils.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from pathlib import Path 
+from dotenv import load_dotenv 
+
+def setting():
+    load_dotenv()
+    read_mdfile("./static/css/css.md")
+
+def read_mdfile(filepath:str) -> st.markdown:
+    """markdown 파일을 읽고 markdown으로 작성하는 함수
+
+    Args:
+        filepath (str): markdown 파일 경로
+
+    Returns:
+        str: markdown 파일에서 추출된 텍스트
+    """
+    file = Path(filepath)
+    
+    if not file.is_file():
+        file_text = f"[ERROR] 파일 경로를 찾을 수 없습니다.(INPUT PATH: {filepath})"
+    else:
+        file_text = file.read_text(encoding="utf-8")
+
+    return st.markdown(file_text, unsafe_allow_html=True)
+
+def vertical_space(size:int):
+    """Create vertical blank spaces
+
+    Args:
+        size (int): height
+    """
+    st.container(height=size, border=False)
+
+def st_textbox(text, fontsize=1, style=None):
+    if style=="list":
+        html = f"<li style='font-size:{fontsize}rem; margin-left: 2px;'>{text}</li>"
+    else:
+        html = f"<div style='font-size:{fontsize}rem;'>{text}</div>"
+
+    return st.markdown(html, unsafe_allow_html=True)
